c428708d68417c4897f5103d81aad074678ef491
Rodrigo Bayá <rbaya@xmartlabs.com>
2020-09-23 18:32:35 -0300

    Use rules of custom images by Kubeflow

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PYTHONPATH=$SPARK_HOME/python:$(echo $SPARK_HOME/python/lib/py4j-*-src.zip)
-exec jupyter notebook --ip=0.0.0.0 --port=8080 --no-browser
+exec jupyter notebook --notebook-dir=/home/jovyan --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=$NB_PREFIX
