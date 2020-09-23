314a870b049038c853d4f050d8eb1e593a07e55f
Rodrigo Bayá <rbaya@xmartlabs.com>
2020-09-23 18:23:12 -0300

    Update port when executing jupyter nootebook

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export PYTHONPATH=$SPARK_HOME/python:$(echo $SPARK_HOME/python/lib/py4j-*-src.zip)
-exec jupyter notebook --ip=0.0.0.0 --no-browser
+exec jupyter notebook --ip=0.0.0.0 -port=8080 --no-browser
